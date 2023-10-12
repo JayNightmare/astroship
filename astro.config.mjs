7958ddf8c6d022eb8093f49f8581a6ec0c2a6f1a
JayNightmar3 <jordanbell321@gmail.com>
2023-10-12 15:15:33 +0100

    possible fix?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
@@ -15,4 +16,11 @@ export default defineConfig({
     mdx(),
     sitemap(),
   ],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
+
